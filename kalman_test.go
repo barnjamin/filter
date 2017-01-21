d6a75acb8a54d50c4e50e0d67f1b52524bce6222
Ben guidarelli <ben.guidarelli@gmail.com>
2017-01-20 19:07:11 -0500

    adding example

--- kalman_test.go
+++ kalman_test.go
@@ -1,7 +1,32 @@
 package kalman
 
-import "testing"
+import (
+	"kalman"
+	"log"
+	"math"
+	"math/rand"
+)
 
-func TestKalman(t *testing.T) {
+func Example() {
+
+	var (
+		q            = 1e-5
+		actualValue  = -0.377
+		r            = math.Pow(0.01, 2)
+		initialError = 0
+		initialValue = 0
+	)
+
+	k := kalman.New(q, r, initialError, initialValue)
+
+	measuredValue := func() float64 {
+		return (rand.NormFloat64()*0.3 + actualValue)
+	}
+
+	for x := 0; x < 50; x++ {
+		measured := measuedValue()
+		estimate := k.Feed(measured)
+		log.Printf("Actual: %.3f Measured: %.3f New Estimate: %.3f", actualValue, measured, estimate)
+	}
 
 }
